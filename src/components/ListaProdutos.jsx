1807a391168fb6c1baaff6ee44a0828882d5dad1
Gabriel <shadowninja2093@gmail.com>
2023-09-19 12:01:17 -0300

    Criando uma página de login usando react

---
+++ src/components/ListaProdutos.jsx
@@ -0,0 +1,8 @@
+export const ListaProdutos =[
+
+    {id: 1, nome: 'Teclado Gamer', desc: 'Mecânico', preco: 200},
+    {id: 1, nome: 'Mouse Gamer', desc: 'Sem fio', preco: 300},
+    {id: 1, nome: 'Cadeira Gamer', desc: 'Top', preco: 2200},
+    {id: 1, nome: 'Fone Gamer', desc: 'Melhor som', preco: 1500},
+    {id: 1, nome: 'Mousepad Gamer', desc: 'plástico', preco: 100},
+]
